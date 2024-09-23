5d4dd51d5f292cf69c28d4ea1c9e33a0bcdf0574
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-09-24 01:37:41 +0600

    static boolean checkEquationValidity(String equation) COMPLETED

--- DataStracture/Stack/Stack.java
+++ DataStracture/Stack/Stack.java
@@ -1,10 +1,10 @@
 package DataStracture.Stack;
 
 class Stack_SLL {
-    private static class data_Stack {
+    private static class Data {
         private int data;
 
-        public data_Stack(int data) {
+        public Data(int data) {
             this.data = data;
         }
 
@@ -23,23 +23,23 @@ class Stack_SLL {
     } // copied from SLL
 
     private static class Node_Stack {
-        private data_Stack data;
+        private Data data;
         private Node_Stack next;
 
-        public Node_Stack(data_Stack data, Node_Stack next) {
+        public Node_Stack(Data data, Node_Stack next) {
             this.data = data;
             this.next = next;
         }
 
-        public Node_Stack(data_Stack data) {
+        public Node_Stack(Data data) {
             this.data = data;
         }
 
-        public data_Stack getDataObj() {
+        public Data getDataObj() {
             return data;
         }
 
-        public void setData(data_Stack data) {
+        public void setData(Data data) {
             this.data = data;
         }
 
@@ -105,7 +105,7 @@ class Stack_SLL {
         return current;
     } // completed
 
-    void push(data_Stack d) { // copied from SLL
+    void push(Data d) { // copied from SLL
         Node_Stack new_node = new Node_Stack(d, null);
         if (head == null) {
             head = new_node;
@@ -135,7 +135,7 @@ class Stack_SLL {
         int[] arr2 = {1, 2, 3, 4, 5};
 
         for (int i = 0; i < arr2.length; i++) {
-            list2.push(new data_Stack(arr2[i]));
+            list2.push(new Data(arr2[i]));
         }
 
         list2.printStack();
@@ -157,108 +157,153 @@ class Stack_SLL {
 
 
 class Stack_Array {
-    private static class data_Stack {
-        private int data;
+    private static class Data {
+        //        private int data;
+        private char data;
 
-        public data_Stack(int data) {
+        //        public Data(int data) {
+//            this.data = data;
+//        }
+        public Data(char data) {
             this.data = data;
         }
 
-        public int getData() {
+        public char getData() {
             return data;
         }
 
-        public void setData(int data) {
+        public void setData(char data) {
             this.data = data;
         }
+//        public void setData(int data) {
+//            this.data = data;
+//        }
 
         @Override
         public String toString() {
-            return STR."\{this.data}";
+            return STR."\{this.data} ";
         }
     } // copied from SLL
 
+    private Data[] stack;
+    //    private int size = 100; /*stack_capacity; default 100*/
+    private static final int size = 100; /*stack_capacity; default 100*/
+    private int top_idx = -1; /*index of the top element*/
 
-    private data_Stack[] stacks;
-    private int limit_of_stack = 100;
-    private int size;
-
-    Stack_Array(int stack_size) {
-        this.limit_of_stack = stack_size;
-        stacks = new data_Stack[this.limit_of_stack];
-        this.size = 0;
+    Stack_Array(int stack_capacity) {
+//        size = stack_capacity; // omitted for applying farhan sir's method
+        stack = new Data[stack_capacity];
     }
 
     Stack_Array() {
-//        this(limit_of_stack); // ------------------------------------- kaj kore na keno
-        stacks = new data_Stack[this.limit_of_stack];
-        this.size = 0;
-
+        this(size);
     }
+
+    /*
+    Stack_Array() {
+        stack = new Data[size];
+    }
+    */
 
     boolean isEmpty() {
-        return size == 0;
+        return top_idx < 0; /*zero can be an index of any array*/
     }
 
-    public int getCurrentSize() {
-        return size;
+    int getCapacity() { /*get the max capacity of the stack*/ // ------------ CAREFUL; dont mix with getSize
+        return stack.length;
     }
 
-    void push(int d) { // add last
-        if (size >= limit_of_stack) {
-            System.out.println("Stack Overflow !!! int " + d + " failed to add");
+    int getSize() { /*get the total number of elements*/ // ---------------------- CAREFUL
+        return top_idx + 1;
+    }
+
+    void push(Data d) { // add last
+        if (top_idx >= getCapacity()) {
+            System.out.println(STR."Stack Overflow !!! int \{d} failed to add");
         } else {
-            stacks[size++] = new data_Stack(d);
-            System.out.println(STR."data \{d} has been pushed to stack");
+            top_idx++;
+            stack[top_idx] = d;
+            System.out.println(STR."INSERTION: data \{d} has been pushed to stack");
         }
     } //
 
-    data_Stack pop() { // remove last
-        if (!isEmpty()) {
-            data_Stack return_value = stacks[size - 1];
-            stacks[size - 1] = null;
-            size--;
-            System.out.println(STR."data \{} has been popped");
+    Data pop() { // remove last
+        if (isEmpty()) {
+            System.out.println("DELETION: Stack is empty");
+            return null;
+        } else {
+            Data return_value = stack[top_idx];
+            stack[top_idx] = null;
+            top_idx--;
+            System.out.println(STR."DELETION: data \{return_value} has been popped");
             return return_value;
         }
-        return null;
     }
 
-    boolean printStack() {
+    Data getTop() { // remove last
+        if (isEmpty()) {
+            return null;
+        } else {
+            Data return_value = stack[top_idx];
+            stack[top_idx] = null;
+            return return_value;
+        }
+    }
+
+    void printStack() {
         /*
          * returns true if stack has element,
          * returns false if stack is empty
          **/
         if (isEmpty()) {
-            System.out.println("Stack is empty");
-            return false;
+            System.out.println("PRINTING: Stack is empty");
         } else {
-            System.out.print("Printing stack : ( ");
-            for (int i = 0; i < size; i++) {
-                System.out.print(stacks[i]);
+            System.out.print("PRINTING: ( ");
+            for (int i = 0; i <= top_idx; i++) {
+                System.out.print(STR."\{stack[i]}");
             }
             System.out.println(")");
-            return true;
         }
     } // completed
 
+    static boolean checkEquationValidity(String equation) {
+        Stack_Array stack_of_braces = new Stack_Array();
+//        for (int i = 0; equation.charAt(i) != '\0'; i++) {
+        for (int i = 0; i < equation.length(); i++) {
+            char ch = equation.charAt(i);
+            if (ch == '[' || ch == '{' || ch == '(') { /*opening, pushing*/
+                stack_of_braces.push(new Data(ch));
+                continue;
+            } else if (ch == ']' || ch == '}' || ch == ')') {
+                char top_ch = stack_of_braces.getTop().getData();
+                if ((top_ch == '[' && ch == ']') || (top_ch == '{' && ch == '}') || (top_ch == '(' && ch == ')')) {
+                    stack_of_braces.pop();
+                } else {
+                    System.out.println("Invalid Equation");
+                    return false;
+                }
+            }
+        }
+        System.out.println("Valid Equation");
+        return true;
+    } // DONE
+
     public static void main(String[] args) {
-        Stack_Array list2 = new Stack_Array();
+        Stack_Array list1 = new Stack_Array();
+        Stack_Array list2 = new Stack_Array(50);
         int[] arr2 = {1, 2, 3, 4, 5, 33};
 
-        for (int i = 0; i < arr2.length; i++) {
-            list2.push(arr2[i]);
-        }
-
-        list2.printStack();
-
-        list2.pop();
-
-        list2.printStack();
-
+//        for (int i = 0; i < arr2.length; i++) {
+////            list1.push(arr2[i]);
+//            list2.push(arr2[i]);
+//        }
+//        String s = "[(A + B) - {C - D}] - [F - G]";
+        String s = "[(A +} B) - {C - D}] - [F - G]";
+        boolean b = checkEquationValidity(s);
+        System.out.println(b);
 
     }
-}
+} // WORKING
 
 
 class Stack_DLL {
