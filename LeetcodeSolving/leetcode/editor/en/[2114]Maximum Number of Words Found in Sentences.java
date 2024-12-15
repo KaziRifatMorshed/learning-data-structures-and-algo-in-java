a897bb248aee0b83ef515b420c8688165ecbb60d
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-12-16 01:22:45 +0600

    Solving some Leetcode in TF

--- LeetcodeSolving/leetcode/editor/en/[2114]Maximum Number of Words Found in Sentences.java
+++ LeetcodeSolving/leetcode/editor/en/[2114]Maximum Number of Words Found in Sentences.java
@@ -1,4 +1,5 @@
-package LeetcodeSolving.leetcode.editor.en;//A sentence is a list of words that are separated by a single space with no
+package LeetcodeSolving.leetcode.editor.en;
+//A sentence is a list of words that are separated by a single space with no
 //leading or trailing spaces.
 //
 // You are given an array of strings sentences, where each sentences[i]
