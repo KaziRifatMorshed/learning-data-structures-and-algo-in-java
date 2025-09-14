dd1a5f3cff8229c6c7bb8561793006ed1339189a
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2025-09-14 14:05:18 +0600

    Create PriorityPeemptive.java

---
+++ OperatingSystems/SchedulingAlgo/PriorityPeemptive.java
@@ -0,0 +1,107 @@
+package OperatingSystems.SchedulingAlgo;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class PriorityPreemptive {
+    ArrayList<Process> processes;
+    int processCount = 0;
+    ArrayList<String> ganttChart = new ArrayList<>();
+
+    PriorityPreemptive() {
+        processes = new ArrayList<>();
+    }
+
+    void addProcess(Process p) {
+        p.status = Status.Ready;
+        processes.add(p);
+        processCount++;
+    }
+
+    void exec() {
+        int currentTime = 0; // nano-sec
+        PriorityQueue<Process> queue = new PriorityQueue<>(
+                Comparator.comparingInt((Process process) -> process.priority)
+                        .thenComparingInt(process -> process.pid)); // THIS WORKS
+
+        for (int servedProcess = 0; servedProcess < processCount; currentTime++) {
+            for (Process p : processes) {
+                if (currentTime == p.arrivalTime && !queue.contains(p)) queue.add(p);
+            }
+
+            Process currentlyExecuting = queue.peek();
+            currentlyExecuting.status = Status.Running;
+            ganttChart.add("p" + currentlyExecuting.pid);
+
+            if (currentlyExecuting.remainingBurstTime == currentlyExecuting.burstTime) {
+                currentlyExecuting.startingTime = currentTime;
+            }
+
+            for (Process p : queue) {
+                if (p.pid != currentlyExecuting.pid) {
+                    p.status = Status.Waiting;
+                    p.waitingTime++;
+                }
+            }
+
+            if (currentlyExecuting.remainingBurstTime > 0) {
+                currentlyExecuting.remainingBurstTime--;
+                currentlyExecuting.status = Status.Waiting;
+            }
+            if (currentlyExecuting.remainingBurstTime == 0) {
+                currentlyExecuting.endingTime = currentTime;
+
+                currentlyExecuting.status = Status.Terminated;
+                queue.poll();
+                servedProcess++;
+            }
+        }
+
+        System.out.println("Total time taken = " + currentTime);
+        System.out.println("Waiting time for all processes:");
+        double avgWaitingTime = 0;
+        for (Process process : processes) {
+            System.out.println("pid = " + process.pid + ", waiting time = " + process.waitingTime);
+            avgWaitingTime += (double) process.waitingTime;
+        }
+        avgWaitingTime /= (double) processes.size();
+        System.out.println("Avg Waiting Time = " + avgWaitingTime);
+        System.out.println("Throughput = " + (double) (currentTime / processCount));
+
+        printGanttChart();
+
+        debug();
+    }
+
+    void printGanttChart() {
+        System.out.println("\nPrinting Gantt Chart:");
+        for (String s : ganttChart) {
+            System.out.print(s + "|");
+        }
+    }
+
+    void debug() {
+        System.out.println("\n\nPrinting all process details:");
+        for (Process p : processes) {
+            System.out.println(p);
+        }
+    }
+
+    public static void main(String[] args) {
+        Process p1 = new Process(1,3, 0, 10);
+        Process p2 = new Process(2,1, 0, 1);
+        Process p3 = new Process(3,4, 0, 2);
+        Process p4 = new Process(4,5, 0, 1);
+        Process p5 = new Process(5,2, 0, 5);
+
+        PriorityPreemptive priorityPreemptive = new PriorityPreemptive();
+        priorityPreemptive.addProcess(p1);
+        priorityPreemptive.addProcess(p2);
+        priorityPreemptive.addProcess(p3);
+        priorityPreemptive.addProcess(p4);
+        priorityPreemptive.addProcess(p5);
+        priorityPreemptive.exec();
+    }
+
+} // should work
