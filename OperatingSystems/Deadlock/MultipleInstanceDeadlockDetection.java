e02a69820279ff7fa4f987628dffe88b7ba97ed6
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2025-10-19 01:18:50 +0600

    ksiu korsi

--- OperatingSystems/Deadlock/MultipleInstanceDeadlockDetection.java
+++ OperatingSystems/Deadlock/MultipleInstanceDeadlockDetection.java
@@ -1,8 +1,51 @@
 package OperatingSystems.Deadlock;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 class MultipleInstanceDeadlockDetection {
 
-    void readFile() {
+    int numProcesses; // row
+    int numResources; // col
+    final String prefixPath = "OperatingSystems/Deadlock/";
+    int[] existenceArr;
+    int[] availableArr;
+    int[][] currentAllocMtx;
+    int[][] reqMtx;
+
+    void readFile() throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(prefixPath + "parameters.txt"));
+        numProcesses = scanner.nextInt();
+        numResources = scanner.nextInt();
+
+        scanner = new Scanner(new File(prefixPath + "existence.txt"));
+        existenceArr = new int[numResources];
+        for (int i = 0; i < numResources; i++) {
+            existenceArr[i] = scanner.nextInt();
+        }
+
+        scanner = new Scanner(new File(prefixPath + "available.txt"));
+        availableArr = new int[numResources];
+        for (int i = 0; i < numResources; i++) {
+            availableArr[i] = scanner.nextInt();
+        }
+
+        scanner = new Scanner(prefixPath + "currentAllocMtx.txt");
+        currentAllocMtx = new int[numProcesses][numResources];
+        for (int r = 0; r < numProcesses; r++) {
+            for (int c = 0; c < numResources; c++) {
+                currentAllocMtx[r][c] = scanner.nextInt();
+            }
+        }
+
+        scanner = new Scanner(prefixPath + "reqMtx.txt");
+        reqMtx = new int[numProcesses][numResources];
+        for (int r = 0; r < numProcesses; r++) {
+            for (int c = 0; c < numResources; c++) {
+                reqMtx[r][c] = scanner.nextInt();
+            }
+        }
 
     }
 
@@ -10,7 +53,7 @@ class MultipleInstanceDeadlockDetection {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         MultipleInstanceDeadlockDetection algo = new MultipleInstanceDeadlockDetection();
         algo.readFile();
         algo.exec();
