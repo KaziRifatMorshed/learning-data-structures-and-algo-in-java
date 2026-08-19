5e2218c1a92a182a7ae7da9b371db90f6633a4a5
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2026-08-19 10:37:26 +0600

    a

--- test.java
+++ test.java
@@ -1,13 +1,19 @@
 class test {
 
     public static void main(String[] args) {
-        int a = 1534236469;
-        int aa = Integer.MAX_VALUE;
-        int b = -2147483648;
-        System.out.println(214748364 < 964632435);
-        int x = -3;
-        x *= -1;
-        System.out.println(x);
+//        int a = 1534236469;
+//        int aa = Integer.MAX_VALUE;
+//        int b = -2147483648;
+//        System.out.println(214748364 < 964632435);
+//        int x = -3;
+//        x *= -1;
+
+//        int c = 'Z';
+//        System.out.println(c);
+
+        String s = "name hello world";
+        s = s.replaceAll("", "~");
+        System.out.println(s);
     }
 
 }
