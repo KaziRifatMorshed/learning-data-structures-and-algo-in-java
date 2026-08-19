77ee636d13c60bcefd2f2025f959dc653fbaa818
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2026-08-19 10:37:49 +0600

    a

--- CompilerDesign/Lab1/output.c
+++ CompilerDesign/Lab1/output.c
@@ -1,22 +1,6 @@
-#include "stdio.h"
-#include "string.h"
-void StrRev(char *str, int ci) {
- if (str[ci] == '\0') {
- return;
- } else {
- StrRev(str, ci + 1);
- }
- printf("%c", str[ci]);
-}
-char OutOneChar(char *str, int ci) {
- OutOneChar(str, ci + 1);
- if (str[ci] == '\0') {
- return '\0';
- } else {
- return str[ci + 1];
- }
-}
-int main(void) {
- char str[10] = "HElloWW";
- StrRev(str, 0);
+void main() {
+ int i,
+ j = 5, k = 10;
+ j = j + k; 
+ printf("%d", i);
 }
